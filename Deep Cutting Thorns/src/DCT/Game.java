b3e374347e37868aa28f187b62bc10ce41bedd9c
Einar4 <andreasbabo6@gmail.com>
2021-10-26 23:09:48 +0200

    0003-done method start

--- Deep Cutting Thorns/src/DCT/Game.java
+++ Deep Cutting Thorns/src/DCT/Game.java
@@ -2,19 +2,41 @@ package DCT;
 
 public class Game {
 
-	public Game() {
+	private boolean running;
+	private Display display;
+	private Thread thread;
 
+	public Game(String title, int width, int height) {
+		this.display = new Display(title, width, height);
+		this.running = false;
 	}
 
 	public void initialize() {
-
+		// TODO
 	}
 
 	public void update() {
-		
+		// TODO
 	}
 
 	public void render() {
-		
+		// TODO
+	}
+
+	public void run() {
+		// TODO
+	}
+
+	public synchronized void start() {
+		if (this.running) {
+			return;
+		}
+		this.thread = new Thread();
+		this.running = true;
+		this.thread.start();
+	}
+
+	public void stop() {
+		// TODO
 	}
 }
