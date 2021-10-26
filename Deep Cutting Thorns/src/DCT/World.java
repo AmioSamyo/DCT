9c3293a4a5937f38ba0ade73341d18a3838dba96
Alessio <alessiomalfissi99@outlook.it>
2021-10-26 22:42:22 +0200

    0004 - Getters for columns and rows

--- Deep Cutting Thorns/src/DCT/World.java
+++ Deep Cutting Thorns/src/DCT/World.java
@@ -5,7 +5,7 @@ import java.awt.Graphics;
 public class World {
 	
 	private int[][] tiles;
-	private int width, height;
+	private int columns, rows;
 	
 	public World(String path) {
 		this.loadWorld(path);
@@ -19,17 +19,25 @@ public class World {
 		
 	}
 	
+	public int getColumns() {
+		return this.columns;
+	}
+	
+	public int getRows() {
+		return this.rows;
+	}
+	
 	private void loadWorld(String path) {
 		String file = Utils.loadFileAsString(path);
 		String[] tokens = file.split("\\s+");
 		
-		this.width = Utils.parseInt(tokens[0]);
-		this.height = Utils.parseInt(tokens[1]);
-		this.tiles = new int[this.width][this.height];
+		this.columns = Utils.parseInt(tokens[0]);
+		this.rows = Utils.parseInt(tokens[1]);
+		this.tiles = new int[this.columns][this.rows];
 		
-		for(int y = 0; y < this.height; y++) {
-			for(int x = 0; x < this.width; x++) {
-				tiles[x][y] = Utils.parseInt(tokens[(x + y * this.width) + 4]);
+		for(int y = 0; y < this.rows; y++) {
+			for(int x = 0; x < this.columns; x++) {
+				tiles[x][y] = Utils.parseInt(tokens[(x + y * this.columns) + 4]);
 			}
 		}
 	}
