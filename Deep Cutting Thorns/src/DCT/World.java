70470cf77749f5ff84eaa369dc008e8a6683c517
Alessio <alessiomalfissi99@outlook.it>
2021-10-26 22:33:23 +0200

    0004 - loadWorld method: RED

--- Deep Cutting Thorns/src/DCT/World.java
+++ Deep Cutting Thorns/src/DCT/World.java
@@ -1,9 +1,27 @@
 package DCT;
 
 public class World {
-
-	public World() {
-
+	
+	private int[][] tiles;
+	private int width, height;
+	
+	public World(String path) {
+		this.loadWorld(path);
+	}
+	
+	private void loadWorld(String path) {
+		String file = Utils.loadFileAsString(path);
+		String[] tokens = file.split("\\s+");
+		
+		this.width = Utils.parseInt(tokens[0]);
+		this.height = Utils.parseInt(tokens[1]);
+		this.tiles = new int[this.width][this.height];
+		
+		for(int y = 0; y < this.height; y++) {
+			for(int x = 0; x < this.width; x++) {
+				tiles[x][y] = Utils.parseInt(tokens[(x + y * this.width) + 4]);
+			}
+		}
 	}
 
 }
