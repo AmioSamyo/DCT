a0085edc12027e55b1ccaea7322336638b43b6b4
Loggi <flaviog2@hotmail.it>
2021-10-26 22:51:18 +0200

    0002 - setup methods

---
+++ Deep Cutting Thorns/src/DCT/Display.java
@@ -0,0 +1,24 @@
+package DCT;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class Display {
+	
+	private JFrame frame;
+	private Canvas canvas;
+	
+
+	public Display(String title, int width, int height) {
+		//take jframe and canvas and put togheter
+	}
+	
+	public JFrame NewJFrame() {
+		return frame;
+	}
+	
+	public Canvas NewCanvas() {
+		return canvas;
+	}
+}
