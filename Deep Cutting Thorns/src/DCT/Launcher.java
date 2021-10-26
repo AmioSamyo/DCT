5f977dfd0f03e205c31608e69499b16b3a1fdab4
Alessio <alessiomalfissi99@outlook.it>
2021-10-26 20:27:29 +0200

    Little refactor

--- Deep Cutting Thorns/src/DCT/Launcher.java
+++ Deep Cutting Thorns/src/DCT/Launcher.java
@@ -1,12 +1,13 @@
 package DCT;
 
 public class Launcher {
-	
-	private static final int WIDTH = 1200, HEIGHT = 700;
+
+	private static final int WIDTH = 1200;
+	private static final int HEIGHT = 700;
 	private static final String TITLE = "Deep Cutting Thorns";
 
 	public static void main(String[] args) {
-		
+		System.out.println(TITLE + " " + WIDTH + " " + HEIGHT);
 	}
 
 }
