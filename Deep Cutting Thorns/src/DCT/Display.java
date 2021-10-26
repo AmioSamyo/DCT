a377080c931f2b193e66d012e7d6fa93e4c52e15
Loggi <flaviog2@hotmail.it>
2021-10-26 23:44:48 +0200

    0002 - method FrameSetter

--- Deep Cutting Thorns/src/DCT/Display.java
+++ Deep Cutting Thorns/src/DCT/Display.java
@@ -5,20 +5,46 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 
 public class Display {
-	
+
 	private JFrame frame;
 	private Canvas canvas;
-	
+
+	private String title;
+	private int width, height;
 
 	public Display(String title, int width, int height) {
-		//take jframe and canvas and put togheter
+		this.title = title;
+		this.width = width;
+		this.height = height;
+		CreateDisplay();
 	}
-	
-	public JFrame NewJFrame() {
+
+	public void CreateDisplay() {
+		frame = FrameSetter();
+		canvas = CanvasSetter();
+	}
+
+	public JFrame getJFrame() {		
 		return frame;
 	}
-	
-	public Canvas NewCanvas() {
+
+	public Canvas getCanvas() {
+		return canvas;
+	}
+
+	private JFrame FrameSetter() {
+		frame = new JFrame(title);
+		
+		frame.setSize(width, height);
+		frame.setResizable(false);
+		frame.setVisible(true);
+		frame.setFocusable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		return frame;
+	}
+
+	private Canvas CanvasSetter() {
+
 		return canvas;
 	}
 }
