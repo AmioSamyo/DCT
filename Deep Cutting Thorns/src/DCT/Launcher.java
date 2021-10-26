c776488f55bcead6f06e4dd5cea6ab3ef9f9bad0
Alessio <alessiomalfissi99@outlook.it>
2021-10-26 19:20:19 +0200

    Delevop branch setup => production

--- Deep Cutting Thorns/src/DCT/Launcher.java
+++ Deep Cutting Thorns/src/DCT/Launcher.java
@@ -1,10 +1,12 @@
 package DCT;
 
 public class Launcher {
+	
+	private static final int WIDTH = 1200, HEIGHT = 700;
+	private static final String TITLE = "Deep Cutting Thorns";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Funzia
+		
 	}
 
 }
