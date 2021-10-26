0316c29603374c1df4047a8e06c3947ae9a8994a
AmioSamyo <49532124+AmioSamyo@users.noreply.github.com>
2021-10-26 23:15:00 +0200

    Alessio/dct 0004 sb => develop (#2)
    * 0004 - Setup World class
    
    * 0004 - loadWorld method: RED
    
    * 0004 - Utils class => loadWorld moethod:GREEN
    
    * 0004 - update & render methods setupped
    
    * 0004 - Getters for columns and rows
    
    * 0004 - Refactor: clean up code
    
    * 0004 - Utils.parseInt test
    
    * 0004 - Utils.parseInt, bad path
    
    * 0004 - fixed value in loadWorld

---
+++ Deep Cutting Thorns/src/DCT/World.java
@@ -0,0 +1,46 @@
+package DCT;
+
+import java.awt.Graphics;
+
+public class World {
+
+	private int[][] tiles;
+	private int columns, rows;
+
+	public World(String path) {
+		this.loadWorld(path);
+	}
+
+	public void render(Graphics g) {
+		// TODO
+	}
+
+	public void update() {
+		// TODO
+	}
+
+	public int getColumns() {
+		return this.columns;
+	}
+
+	public int getRows() {
+		return this.rows;
+	}
+
+	private void loadWorld(String path) {
+		String file = Utils.loadFileAsString(path);
+		String[] tokens = file.split("\\s+");
+		int toBeSkipped = 2;
+
+		this.columns = Utils.parseInt(tokens[0]);
+		this.rows = Utils.parseInt(tokens[1]);
+		this.tiles = new int[this.columns][this.rows];
+
+		for (int y = 0; y < this.rows; y++) {
+			for (int x = 0; x < this.columns; x++) {
+				this.tiles[x][y] = Utils.parseInt(tokens[(x + (y * this.columns)) + toBeSkipped]);
+			}
+		}
+	}
+
+}
---
+++ Deep Cutting Thorns/src/Test/UtilsTest.java
@@ -0,0 +1,24 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+import DCT.Utils;
+
+class UtilsTest {
+
+	@DisplayName("Should convert a String to Int")
+	@Test
+	void testParseInt() {
+		assertEquals(1, Utils.parseInt("1"));
+		assertEquals(89, Utils.parseInt("89"));
+		assertEquals(-3, Utils.parseInt("-3"));
+	}
+	
+	@DisplayName("Should fail to convert a String to Int and return 0")
+	@Test
+	void testParseIntFail() {
+		assertEquals(0, Utils.parseInt("One"));
+	}
+
+}
