ce5f668a5ef5c24a665cbbdc9711033f1efc99a4
Alessio <alessiomalfissi99@outlook.it>
2021-10-26 22:50:52 +0200

    0004 - Utils.parseInt test

---
+++ Deep Cutting Thorns/src/Test/UtilsTest.java
@@ -0,0 +1,18 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+import DCT.Utils;
+
+class UtilsTest {
+
+	@DisplayName("Should convert a String to Int")
+	@Test
+	void testParseInt() {
+		assertEquals(1, Utils.parseInt("1"));
+		assertEquals(89, Utils.parseInt("89"));
+		assertEquals(-3, Utils.parseInt("-3"));
+	}
+
+}
