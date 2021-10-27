a5ffbb8db97607096bcb5037777886c18f12d5c4
Loggi <flaviog2@hotmail.it>
2021-10-27 22:56:06 +0200

    0006 - fixed import and method's name

--- Deep Cutting Thorns/src/DCT/ImageLoader.java
+++ Deep Cutting Thorns/src/DCT/ImageLoader.java
@@ -3,18 +3,23 @@ package DCT;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+
 import javax.imageio.ImageIO;
 
 public class ImageLoader {
-
-	public static BufferedImage load(String path) {
+	
+	public static BufferedImage imageLoad(String path) {
+		
 		BufferedImage img = null;
+		
 		try {
 			img = ImageIO.read(new File(path));
 		}
 		catch(IOException e){
-			
+			e.printStackTrace();
+			//TO DOO
+			return img;
 		}
-		return img;
+		return img;		
 	}
 }
