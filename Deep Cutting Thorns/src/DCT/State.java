21593eea8a9b918b6d9705bd983942a960409f24
Alessio <alessiomalfissi99@outlook.it>
2021-10-27 20:03:00 +0200

    0011 - getter and setter for currentState

--- Deep Cutting Thorns/src/DCT/State.java
+++ Deep Cutting Thorns/src/DCT/State.java
@@ -3,8 +3,18 @@ package DCT;
 import java.awt.Graphics;
 
 public abstract class State {
-	
+
+	private static State currentState = null;
+
 	public abstract void update();
-	
+
 	public abstract void render(Graphics g);
+
+	public State getCurrentState() {
+		return State.currentState;
+	}
+
+	public void setCurrentState(State s) {
+		State.currentState = s;
+	}
 }
