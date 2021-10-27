80db2d6050764b3b99440ec9af8c29a34ec8564d
Einar4 <andreasbabo6@gmail.com>
2021-10-27 22:51:23 +0200

    launcher setup

--- Deep Cutting Thorns/src/DCT/Launcher.java
+++ Deep Cutting Thorns/src/DCT/Launcher.java
@@ -7,7 +7,10 @@ public class Launcher {
 	private static final String TITLE = "Deep Cutting Thorns";
 
 	public static void main(String[] args) {
-		System.out.println(TITLE + " " + WIDTH + " " + HEIGHT);
+
+		Game game = new Game(TITLE, WIDTH, HEIGHT);
+		game.start();
+
 	}
 
 }
