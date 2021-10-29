85ee10cb34b366fda07c5bb86a5aea90ca3dcccc
Einar4 <andreasbabo6@gmail.com>
2021-10-29 13:59:34 +0200

    0007-lint

--- Deep Cutting Thorns/src/DCT/FontLoader.java
+++ Deep Cutting Thorns/src/DCT/FontLoader.java
@@ -1,26 +1,25 @@
 package DCT;
 
-import  java.awt.Font;
+import java.awt.Font;
 import java.awt.FontFormatException;
 import java.io.File;
 import java.io.IOException;
 
 public class FontLoader {
 
-	public static Font fontLoad(String path,float size) {
-		Font font;
+	public static Font fontLoad(String path, float size) {
+
 		try {
-			font=Font.createFont(Font.TRUETYPE_FONT, new File(path));
-			font.deriveFont(Font.PLAIN, size);
-			return font;
-		}catch(FontFormatException e) {
-			 e.printStackTrace();
+			return Font.createFont(Font.TRUETYPE_FONT, new File(path)).deriveFont(Font.PLAIN, size);
+		} catch (FontFormatException e) {
+			e.printStackTrace();
 			System.exit(-1);
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(-2);
 		}
+
 		return null;
-		
+
 	}
 }
