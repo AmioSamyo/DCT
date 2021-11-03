343f69e8cdb3dfcb723d047ecdfb48a479b392d7
Einar4 <andreasbabo6@gmail.com>
2021-11-03 23:12:37 +0100

    0015-added KeyJustPressed

--- Deep Cutting Thorns/src/DCT/input/KeyManager.java
+++ Deep Cutting Thorns/src/DCT/input/KeyManager.java
@@ -11,32 +11,32 @@ public class KeyManager implements KeyListener {
 
 	public KeyManager() {
 
-		keys = new boolean[256];
-		justPressed = new boolean[keys.length];
-		cantPress = new boolean[keys.length];
+		this.keys = new boolean[256];
+		this.justPressed = new boolean[keys.length];
+		this.cantPress = new boolean[keys.length];
 
 	}
 
 	public void update() {
 
-		for (int i = 0; i < keys.length; i++) {
+		for (int i = 0; i < this.keys.length; i++) {
 
-			if (cantPress[i] && !keys[i]) {
-				cantPress[i] = false;
-			} else if (justPressed[i]) {
-				cantPress[i] = true;
-				justPressed[i] = false;
+			if (this.cantPress[i] && !this.keys[i]) {
+				this.cantPress[i] = false;
+			} else if (this.justPressed[i]) {
+				this.cantPress[i] = true;
+				this.justPressed[i] = false;
 			}
-			if (!cantPress[i] && keys[i]) {
-				justPressed[i] = true;
+			if (!this.cantPress[i] && this.keys[i]) {
+				this.justPressed[i] = true;
 			}
 		}
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		
-		if(e.getKeyCode() < 0 || e.getKeyCode() >= keys.length) 
+
+		if (e.getKeyCode() < 0 || e.getKeyCode() >= this.keys.length)
 			return;
 		this.keys[e.getKeyCode()] = true;
 
@@ -44,16 +44,23 @@ public class KeyManager implements KeyListener {
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		
-		if (e.getKeyCode() < 0 || e.getKeyCode() >= keys.length)
+
+		if (e.getKeyCode() < 0 || e.getKeyCode() >= this.keys.length)
 			return;
 		this.keys[e.getKeyCode()] = false;
-		
+
 	}
-	
+
 	@Override
 	public void keyTyped(KeyEvent e) {
-		
+
+	}
+
+	public boolean keyJustPressed(int keyCode) {
+		if (keyCode < 0 || keyCode >= this.keys.length) {
+			return false;
+		}
+		return this.justPressed[keyCode];
 	}
 
 }
