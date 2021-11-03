9c29b5d8a2f5fdbe743bf50845523e6426a4207f
Einar4 <andreasbabo6@gmail.com>
2021-11-03 22:21:41 +0100

    0015-update updated

--- Deep Cutting Thorns/src/DCT/input/KeyManager.java
+++ Deep Cutting Thorns/src/DCT/input/KeyManager.java
@@ -5,11 +5,29 @@ public class KeyManager {
 	private boolean keys[];
 	private boolean justPressed[];
 	private boolean cantPress[];
-	
+
 	public KeyManager() {
-		keys=new boolean[256];
-		justPressed=new boolean[keys.length];
-		cantPress=new boolean [keys.length];
+
+		keys = new boolean[256];
+		justPressed = new boolean[keys.length];
+		cantPress = new boolean[keys.length];
+
 	}
-	
+
+	public void update() {
+		
+		for (int i = 0; i < keys.length; i++) {
+			
+			if (cantPress[i] && !keys[i]) {
+				cantPress[i] = false;
+			} else if (justPressed[i]) {
+				cantPress[i] = true;
+				justPressed[i] = false;
+			}
+			if (!cantPress[i] && keys[i]) {
+				justPressed[i]=true;
+			}
+		}
+	}
+
 }
