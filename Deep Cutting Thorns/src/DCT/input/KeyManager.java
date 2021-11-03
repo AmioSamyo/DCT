d3332d17ca7b74899fcbe1f6ba73b4c3d3c54e4e
Einar4 <andreasbabo6@gmail.com>
2021-11-03 21:55:51 +0100

    0015-constructor added

--- Deep Cutting Thorns/src/DCT/input/KeyManager.java
+++ Deep Cutting Thorns/src/DCT/input/KeyManager.java
@@ -2,4 +2,14 @@ package DCT.input;
 
 public class KeyManager {
 
+	private boolean keys[];
+	private boolean justPressed[];
+	private boolean cantPress[];
+	
+	public KeyManager() {
+		keys=new boolean[256];
+		justPressed=new boolean[keys.length];
+		cantPress=new boolean [keys.length];
+	}
+	
 }
