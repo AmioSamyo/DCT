8fb9f7e26ed06463c7656d9fca2e3a8362819bb7
Alessio <alessiomalfissi99@outlook.it>
2021-11-04 20:34:34 +0100

    0018 - Tile core Features

--- Deep Cutting Thorns/src/DCT/tile/Tile.java
+++ Deep Cutting Thorns/src/DCT/tile/Tile.java
@@ -1,9 +1,25 @@
 package DCT.tile;
 
+import java.awt.image.BufferedImage;
+import java.awt.Graphics;
+
 public class Tile {
 
-	public Tile() {
-		// TODO Auto-generated constructor stub
+	private int id = 0;
+
+	private BufferedImage texture = null;
+
+	public Tile(BufferedImage texture, int id) {
+		this.texture = texture;
+		this.id = id;
+	}
+
+	public void update() {
+
+	}
+
+	public void render(Graphics g, int x, int y) {
+		g.drawImage(this.texture, x, y, null);
 	}
 
 }
