96cd8d49522d5e124b2f9a1b7f1fcff45d2110ee
Loggi <flaviog2@hotmail.it>
2021-11-04 15:25:40 +0100

    0008 - class rectangle done

--- Deep Cutting Thorns/src/DCT/gfx/Rectangle.java
+++ Deep Cutting Thorns/src/DCT/gfx/Rectangle.java
@@ -1,9 +1,14 @@
 package DCT.gfx;
 
 public class Rectangle {
-
-	public Rectangle() {
-		
+	
+	private int x, y, width, height;	
+	
+	public Rectangle(int x, int y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
 	}
 
 }
