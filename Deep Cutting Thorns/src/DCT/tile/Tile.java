db70841a056da0a4e865d051f0cdad8f8075d215
AmioSamyo <49532124+AmioSamyo@users.noreply.github.com>
2021-11-04 21:16:14 +0100

    Alessio/dct 0018 0019 0026 => develop (#13)
    * 0028 - Tile package
    0018 - Setup Tile class
    
    * 0018 - Tile core Features
    
    * 0019 - Getter for id & solid

---
+++ Deep Cutting Thorns/src/DCT/tile/Tile.java
@@ -0,0 +1,34 @@
+package DCT.tile;
+
+import java.awt.image.BufferedImage;
+import java.awt.Graphics;
+
+public class Tile {
+
+	private int id = 0;
+	private boolean solid = false;
+
+	private BufferedImage texture = null;
+
+	public Tile(BufferedImage texture, int id) {
+		this.texture = texture;
+		this.id = id;
+	}
+
+	public void update() {
+
+	}
+
+	public void render(Graphics g, int x, int y) {
+		g.drawImage(this.texture, x, y, null);
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public boolean isSolid() {
+		return this.solid;
+	}
+
+}
