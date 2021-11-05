92066cfc9f8e63e27bc0723a76f276ca51f69793
Einar4 <andreasbabo6@gmail.com>
2021-11-05 22:24:37 +0100

    0043-added SpriteSheet grass

--- Deep Cutting Thorns/src/DCT/gfx/Assets.java
+++ Deep Cutting Thorns/src/DCT/gfx/Assets.java
@@ -7,6 +7,7 @@ public class Assets {
 	public static BufferedImage grass, grassFlower, grassRock;
 
 	public static void assetInitialize() {
-		// TODO
+
+		SpriteSheet grass = new SpriteSheet(ImageLoader.imageLoad("rsc//32x32_map_Da_Tagliare.png"));
 	}
 }
