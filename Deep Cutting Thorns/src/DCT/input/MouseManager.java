61bfbd4ef2e9dddeefae424077e7807041439869
Loggi <flaviog2@hotmail.it>
2021-11-05 15:48:40 +0100

    0016 - MouseManager class setup

---
+++ Deep Cutting Thorns/src/DCT/input/MouseManager.java
@@ -0,0 +1,54 @@
+package DCT.input;
+
+import java.awt.event.MouseEvent;
+import javax.swing.event.MouseInputListener;
+
+public class MouseManager implements MouseInputListener {
+
+	public MouseManager() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
