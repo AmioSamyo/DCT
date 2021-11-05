2458093f540e53819c9155eba6b7127c9277cae8
Loggi <flaviog2@hotmail.it>
2021-11-05 17:54:20 +0100

    0016 - Pressed and Released method, + new condition in Clicked

--- Deep Cutting Thorns/src/DCT/input/MouseManager.java
+++ Deep Cutting Thorns/src/DCT/input/MouseManager.java
@@ -17,24 +17,44 @@ public class MouseManager implements MouseInputListener {
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		if(e.getButton() == MouseEvent.BUTTON1) {
+		if(e.getButton() == MouseEvent.BUTTON1 && e.getButton() == MouseEvent.BUTTON2) {
 			this.leftClicked = true;
-			return;
+			this.rightClicked = true;
+		}
+		else if(e.getButton() == MouseEvent.BUTTON1) {
+			this.leftClicked = true;
 		}
 		else if(e.getButton() == MouseEvent.BUTTON2) {
 			this.rightClicked = true;
-			return;
 		}
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-
+		if(e.getButton() == MouseEvent.BUTTON1 && e.getButton() == MouseEvent.BUTTON2) {
+			this.leftPressed = true;
+			this.rightPressed = true;
+		}
+		else if(e.getButton() == MouseEvent.BUTTON1) {
+			this.leftPressed = true;
+		}
+		else if(e.getButton() == MouseEvent.BUTTON2) {
+			this.rightPressed = true;
+		}
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		
+		if(e.getButton() == MouseEvent.NOBUTTON) {
+			this.leftPressed = false;
+			this.rightPressed = false;
+		}
+		else if(e.getButton() != MouseEvent.NOBUTTON && e.getButton() != MouseEvent.BUTTON1) {
+			this.leftPressed = false;
+		}
+		else if(e.getButton() != MouseEvent.NOBUTTON && e.getButton() != MouseEvent.BUTTON2) {
+			this.rightPressed = false;
+		}
 	}
 
 	@Override
