42196b7d6a2c96e5ec1eccecdc00c60289c28206
Loggi <flaviog2@hotmail.it>
2021-11-05 17:39:04 +0100

    0016 - mouseClicked method

--- Deep Cutting Thorns/src/DCT/input/MouseManager.java
+++ Deep Cutting Thorns/src/DCT/input/MouseManager.java
@@ -4,50 +4,56 @@ import java.awt.event.MouseEvent;
 import javax.swing.event.MouseInputListener;
 
 public class MouseManager implements MouseInputListener {
+	
+	private int mouseX, mouseY;
+	private boolean leftClicked, leftPressed, rightClicked, rightPressed;
 
 	public MouseManager() {
-		// TODO Auto-generated constructor stub
+		this.leftClicked = false;
+		this.leftPressed = false;
+		this.rightClicked = false;
+		this.rightPressed = false;
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+		if(e.getButton() == MouseEvent.BUTTON1) {
+			this.leftClicked = true;
+			return;
+		}
+		else if(e.getButton() == MouseEvent.BUTTON2) {
+			this.rightClicked = true;
+			return;
+		}
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
 
