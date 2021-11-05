02730dfab21e64e2f84a980cbabf5583d9689fec
Einar4 <andreasbabo6@gmail.com>
2021-11-05 22:28:19 +0100

    0043-added first grass crop

--- Deep Cutting Thorns/src/DCT/gfx/Assets.java
+++ Deep Cutting Thorns/src/DCT/gfx/Assets.java
@@ -2,12 +2,16 @@ package DCT.gfx;
 
 import java.awt.image.BufferedImage;
 
+import DCT.utility.Rectangle;
+
 public class Assets {
 
 	public static BufferedImage grass, grassFlower, grassRock;
 
 	public static void assetInitialize() {
 
-		SpriteSheet grass = new SpriteSheet(ImageLoader.imageLoad("rsc//32x32_map_Da_Tagliare.png"));
+		SpriteSheet grassSheet = new SpriteSheet(ImageLoader.imageLoad("rsc//32x32_map_Da_Tagliare.png"));
+		
+		grass=grassSheet.cropImage(new Rectangle(96, 0, 32,32 ));
 	}
 }
