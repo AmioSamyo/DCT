a44a8ece1af499374d232d5f28cca441e02e74fd
Loggi <flaviog2@hotmail.it>
2021-11-06 14:10:34 +0100

    0043 - loggi - wall asset crop

--- Deep Cutting Thorns/src/DCT/gfx/Assets.java
+++ Deep Cutting Thorns/src/DCT/gfx/Assets.java
@@ -6,12 +6,16 @@ import DCT.utility.Rectangle;
 
 public class Assets {
 
-	public static BufferedImage grass, grassFlower, grassRock;
+	public static BufferedImage grass, grassFlower, grassRock, wall;
 
 	public static void assetInitialize() {
 
 		SpriteSheet grassSheet = new SpriteSheet(ImageLoader.imageLoad("rsc//32x32_map_Da_Tagliare.png"));
-
+		
 		grass = grassSheet.cropImage(new Rectangle(96, 0, 32, 32));
+		
+		SpriteSheet wallSheet = new SpriteSheet(ImageLoader.imageLoad("rsc//walls tile.gif"));
+		
+		wall = wallSheet.cropImage(new Rectangle(0, 0, 32, 32));
 	}
 }
