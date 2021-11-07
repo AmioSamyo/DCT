324584221ffc5ea7efbec72ac5f76f6ff8378331
LoggiExp <92399059+LoggiExp@users.noreply.github.com>
2021-11-07 18:45:06 +0100

    Loggi/dct 0035 sb -> develop (#21)
    * 0035 - Player class setup + constructor
    
    * 0035 - added idle animetion
    
    * 0035 - update() animation
    
    * 0035 - getter currentHealth, die
    
    * 0035 - refactor + render
    
    * 0035 - removed temporary
    
    * 0035 - player implementation
    
    * 0035 - player dimensions definition

---
+++ Deep Cutting Thorns/src/DCT/entity/Player.java
@@ -0,0 +1,64 @@
+package DCT.entity;
+
+import java.awt.Graphics;
+
+import DCT.Facade;
+import DCT.gfx.Animation;
+import DCT.gfx.Assets;
+import DCT.utility.Rectangle;
+
+public class Player extends Creature {
+
+	private int currentHealth;
+	private Animation playerMoveDown, playerMoveRight, playerMoveUp, playerMoveLeft, playerIdle;
+
+	private Animation currentAnimation;
+
+	private static final int PLAYERWIDTH = 704 / 11, PLAYERHEIGHT = 320 / 5;
+	private static final int SCALE = 1;
+
+	public Player(Facade facade, int x, int y) {
+		super(facade, new Rectangle(x, y, PLAYERWIDTH * SCALE, PLAYERHEIGHT * SCALE));
+		initialize();
+	}
+
+	private void initialize() {
+		this.playerMoveDown = new Animation(100, Assets.playerAnimationDown);
+		this.playerMoveRight = new Animation(100, Assets.playerAnimationRight);
+		this.playerMoveUp = new Animation(100, Assets.playerAnimationUp);
+		this.playerMoveLeft = new Animation(100, Assets.playerAnimationLeft);
+		this.playerIdle = new Animation(100, Assets.playerAnimationIdle);
+
+		this.currentAnimation = this.playerIdle;
+	}
+
+	@Override
+	public void update() {
+		if (currentHealth <= 0) {
+			die();
+		}
+		this.playerIdle.update();
+		this.playerMoveUp.update();
+		this.playerMoveRight.update();
+		this.playerMoveLeft.update();
+		this.playerMoveDown.update();
+
+		this.currentAnimation.update();
+	}
+
+	@Override
+	public void render(Graphics g) {
+		g.drawImage(this.currentAnimation.getCurrentFrame(), this.position.getX(), this.position.getY(),
+				this.position.getWidth(), this.position.getHeight(), null);
+	}
+
+	@Override
+	public void die() {
+
+	}
+
+	public int getCurrentHealth() {
+		return this.currentHealth;
+	}
+
+}
