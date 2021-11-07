c7672a8418a03aeaef902353b4dd78670162704f
Loggi <flaviog2@hotmail.it>
2021-11-07 17:40:28 +0100

    0035 - added idle animetion

--- Deep Cutting Thorns/src/DCT/gfx/Assets.java
+++ Deep Cutting Thorns/src/DCT/gfx/Assets.java
@@ -7,7 +7,7 @@ import DCT.utility.Rectangle;
 public class Assets {
 
 	public static BufferedImage grass, flowerGrass, rockGrass, wall;
-	public static BufferedImage[] playerAnimationDown, playerAnimationRight, playerAnimationUp, playerAnimationLeft;
+	public static BufferedImage[] playerAnimationDown, playerAnimationRight, playerAnimationUp, playerAnimationLeft, playerAnimationIdle;
 
 	public static void assetInitialize() {
 
@@ -29,6 +29,7 @@ public class Assets {
 		playerAnimationRight = new BufferedImage[5];
 		playerAnimationUp = new BufferedImage[5];
 		playerAnimationLeft = new BufferedImage[5];
+		playerAnimationIdle = new BufferedImage[5];
 
 		for (int i = 0; i < 5; i++) {
 			playerAnimationDown[i] = playerSheet.cropImage(
@@ -39,6 +40,8 @@ public class Assets {
 					new Rectangle(i * playerAnimWidth, playerAnimHeight * 2, playerAnimWidth, playerAnimHeight));
 			playerAnimationLeft[i] = playerSheet.cropImage(
 					new Rectangle(i * playerAnimWidth, playerAnimHeight * 3, playerAnimWidth, playerAnimHeight));
+			playerAnimationIdle[i] = playerSheet.cropImage(
+					new Rectangle(i * playerAnimWidth, playerAnimHeight * 4, playerAnimWidth, playerAnimHeight));
 		}
 	}
 }
