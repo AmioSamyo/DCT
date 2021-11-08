fddb71be687ade21c391021b3f882d0c05b422f7
Einar4 <andreasbabo6@gmail.com>
2021-11-08 19:03:09 +0100

    0038-fixed

--- Deep Cutting Thorns/src/DCT/Launcher.java
+++ Deep Cutting Thorns/src/DCT/Launcher.java
@@ -6,7 +6,7 @@ public class Launcher {
 
 	private static final int WIDTH = 1200;
 	private static final int HEIGHT = 700;
-	private static final String TITLE = "Deep Cutting Thorns : PENE";
+	private static final String TITLE = "Deep Cutting Thorns";
 
 	public static void main(String[] args) {
 
