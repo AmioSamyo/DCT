fa6ad3846bfe431f55e2eb7bd26fa3c2d5939af5
Einar4 <andreasbabo6@gmail.com>
2021-11-09 20:25:42 +0100

    0044-added addEntity

--- Deep Cutting Thorns/src/DCT/entity/EntityManager.java
+++ Deep Cutting Thorns/src/DCT/entity/EntityManager.java
@@ -1,16 +1,26 @@
 package DCT.entity;
 
+import java.util.ArrayList;
+
 import DCT.Facade;
 
 public class EntityManager {
 
 	private Facade facade;
 	private Player player;
+	private ArrayList<Entity> entityList;
 
 	public EntityManager(Facade facade, Player player) {
 		
 		this.facade=facade;
 		this.player=player;
+		
+		this.entityList=new ArrayList<Entity>();
+	}
+	
+	public void addEntity(Entity e) {
+		
+		this.entityList.add(e);
 	}
 
 }
