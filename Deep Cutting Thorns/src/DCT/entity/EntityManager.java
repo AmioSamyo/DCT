7e6eec18e591cad8f657da355417f097a50a4767
Einar4 <andreasbabo6@gmail.com>
2021-11-09 20:20:12 +0100

    0044-added constructor

--- Deep Cutting Thorns/src/DCT/entity/EntityManager.java
+++ Deep Cutting Thorns/src/DCT/entity/EntityManager.java
@@ -1,9 +1,16 @@
 package DCT.entity;
 
+import DCT.Facade;
+
 public class EntityManager {
 
-	public EntityManager() {
-		// TODO Auto-generated constructor stub
+	private Facade facade;
+	private Player player;
+
+	public EntityManager(Facade facade, Player player) {
+		
+		this.facade=facade;
+		this.player=player;
 	}
 
 }
