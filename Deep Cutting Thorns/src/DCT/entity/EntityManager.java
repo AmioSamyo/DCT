948764bc213211335e747a7335284dc1d9e20ae8
Einar4 <andreasbabo6@gmail.com>
2021-11-09 20:48:13 +0100

    0044-setupped arrayList in render & update

--- Deep Cutting Thorns/src/DCT/entity/EntityManager.java
+++ Deep Cutting Thorns/src/DCT/entity/EntityManager.java
@@ -9,27 +9,34 @@ public class EntityManager {
 
 	private Facade facade;
 	private Player player;
+	
 	private ArrayList<Entity> entityList;
 
 	public EntityManager(Facade facade, Player player) {
-		
-		this.facade=facade;
-		this.player=player;
-		
-		this.entityList=new ArrayList<Entity>();
+
+		this.facade = facade;
+		this.player = player;
+
+		this.entityList = new ArrayList<Entity>();
+		this.addEntity(this.player);
 	}
-	
+
 	public void update() {
-		this.player.update();
+
+		for (int i = 0; i < this.entityList.size(); i++) {
+			this.entityList.get(i).update();
+		}
 	}
-	
+
 	public void render(Graphics g) {
-		this.player.render(g);
+
+		for (int i = 0; i < this.entityList.size(); i++) {
+			this.entityList.get(i).render(g);
+		}
 	}
-	
-	
+
 	public void addEntity(Entity e) {
-		
+
 		this.entityList.add(e);
 	}
 
