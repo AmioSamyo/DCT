ea5b1851c046d9a20d930ae54672e8d2d2aaf711
Alessio <alessiomalfissi99@outlook.it>
2021-11-10 20:24:45 +0100

    0040 - Rectangle intersects method

--- Deep Cutting Thorns/src/DCT/utility/Rectangle.java
+++ Deep Cutting Thorns/src/DCT/utility/Rectangle.java
@@ -1,39 +1,44 @@
 package DCT.utility;
 
 public class Rectangle {
-	
+
 	private int x;
 	private int y;
 	private int width;
-	private int height;	
-	
+	private int height;
+
 	public Rectangle(int x, int y, int width, int height) {
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
 	}
-	
+
+	public boolean intersects(Rectangle r) {
+		return !(r.getX() > this.getX() + this.getWidth() || r.getX() + r.getWidth() < this.getX()
+				|| r.getY() > this.getY() + this.getHeight() || r.getY() + r.getHeight() < this.getY());
+	}
+
 	public int getX() {
 		return this.x;
 	}
-	
+
 	public int getY() {
 		return this.y;
 	}
-	
+
 	public int getWidth() {
 		return this.width;
 	}
-	
+
 	public int getHeight() {
 		return this.height;
 	}
-	
+
 	public void setX(int positionX) {
 		this.x = positionX;
 	}
-	
+
 	public void setY(int positionY) {
 		this.y = positionY;
 	}
