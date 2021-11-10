a0534542c6d19b84638a01b3ab808ebaf90c116c
Einar4 <andreasbabo6@gmail.com>
2021-11-10 23:35:16 +0100

    0046 - added constructor and getter

--- Deep Cutting Thorns/src/DCT/gfx/GameCamera.java
+++ Deep Cutting Thorns/src/DCT/gfx/GameCamera.java
@@ -1,9 +1,26 @@
 package DCT.gfx;
 
+import DCT.Facade;
+
 public class GameCamera {
 
-	public GameCamera() {
-		// TODO Auto-generated constructor stub
+	private int xOffset;
+	private int yOffset;
+
+	private Facade facade;
+
+	public GameCamera(Facade facade) {
+
+		this.facade = facade;
+		this.xOffset = 0;
+		this.yOffset = 0;
+	}
+	
+	public int getXOffset() {
+		return this.xOffset;
 	}
 
+	public int getYOffset() {
+		return this.yOffset;
+	}
 }
