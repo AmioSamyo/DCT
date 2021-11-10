2d83ddf2ccb9bb4e4716dcfda2aa61baf9ea48ea
Alessio <alessiomalfissi99@outlook.it>
2021-11-10 20:25:33 +0100

    0040 - Entity hitBox and checkCollision implementation

--- Deep Cutting Thorns/src/DCT/entity/Entity.java
+++ Deep Cutting Thorns/src/DCT/entity/Entity.java
@@ -1,6 +1,7 @@
 package DCT.entity;
 
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 import DCT.Facade;
 import DCT.utility.Rectangle;
@@ -9,6 +10,7 @@ public abstract class Entity {
 
 	protected Facade facade;
 	protected Rectangle position;
+	protected Rectangle hitBox;
 
 	public Entity(Facade facade, Rectangle position) {
 		this.facade = facade;
@@ -20,6 +22,19 @@ public abstract class Entity {
 	public abstract void render(Graphics g);
 
 	public abstract void die();
+
+	public boolean checkEntityCollisions(int xOffSet, int yOffSet) {
+		ArrayList<Entity> entities = this.facade.getEntityManager().getEntityList();
+
+		for (Entity e : entities) {
+			boolean intersects = e.getCollisionHitBox(0, 0).intersects(this.getCollisionHitBox(xOffSet, yOffSet));
+			if (e != this && intersects) {
+				return true;
+			}
+		}
+
+		return false;
+	}
 
 	public int getPositionX() {
 		return this.position.getX();
@@ -43,5 +58,14 @@ public abstract class Entity {
 
 	public void setY(int positionY) {
 		this.position.setY(positionY);
+	}
+
+	public void setHitBox(int x, int y, int width, int height) {
+		this.hitBox = new Rectangle(x, y, width, height);
+	}
+
+	public Rectangle getCollisionHitBox(int xOffSet, int yOffSet) {
+		return new Rectangle(this.getPositionX() + this.hitBox.getX() + xOffSet,
+				this.getPositionY() + this.hitBox.getY() + yOffSet, this.hitBox.getWidth(), this.hitBox.getHeight());
 	}
 }
