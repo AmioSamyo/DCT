dcb85d9eaa08d63b8405f02172d1a217ef3ef3f5
Loggi <flaviog2@hotmail.it>
2021-11-10 21:39:51 +0100

    0045 - tree initialize, private variables, render

--- Deep Cutting Thorns/src/DCT/entity/statics/Tree.java
+++ Deep Cutting Thorns/src/DCT/entity/statics/Tree.java
@@ -1,11 +1,40 @@
 package DCT.entity.statics;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
 import DCT.Facade;
+import DCT.gfx.Assets;
 
 public class Tree extends StaticEntity {
+	
+	private BufferedImage treeTile;	
+	private int x, y;
+	private static final int TREEWIDTH = 64, TREEHEIGHT = 96;
 
 	public Tree(Facade facade, int x, int y, int width, int height) {
 		super(facade, x, y, width, height);
+		
+		initialize();
+	}
+	
+	private void initialize(){
+		this.treeTile = Assets.tree;
+		this.x = 100;
+		this.y = 100;
 	}
 
+	@Override
+	public void update() {
+	}
+
+	@Override
+	public void render(Graphics g) {
+		g.drawImage(treeTile, this.x, this.y, TREEWIDTH, TREEHEIGHT, null);
+	}
+
+	@Override
+	public void die() {
+		
+	}
 }
