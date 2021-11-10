4658a6ce7d238de7bd871be0b3d0e0109c3ca7d4
Alessio <alessiomalfissi99@outlook.it>
2021-11-10 20:27:33 +0100

    0040 - WallTile isSolid override

--- Deep Cutting Thorns/src/DCT/tile/WallTile.java
+++ Deep Cutting Thorns/src/DCT/tile/WallTile.java
@@ -5,7 +5,12 @@ import DCT.gfx.Assets;
 public class WallTile extends Tile {
 
 	public WallTile(int id) {
-		super (Assets.wall, id);
+		super(Assets.wall, id);
+	}
+
+	@Override
+	public boolean isSolid() {
+		return true;
 	}
 
 }
