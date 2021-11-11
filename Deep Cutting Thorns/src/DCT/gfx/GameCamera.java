c219c233dd125a4aa9bab0830c625b93ce7d05a3
LoggiExp <92399059+LoggiExp@users.noreply.github.com>
2021-11-11 22:39:18 +0100

    Loggi/dct 0047 sb => develop (#28)
    * 0047 - move method
    
    * 0047 - centerOnEntity method
    
    * 0047 - checkBlankSpace method
    
    * 0047 - fixed
    
    * 0047 - this.

--- Deep Cutting Thorns/src/DCT/gfx/GameCamera.java
+++ Deep Cutting Thorns/src/DCT/gfx/GameCamera.java
@@ -1,6 +1,7 @@
 package DCT.gfx;
 
 import DCT.Facade;
+import DCT.entity.Entity;
 
 public class GameCamera {
 
@@ -15,6 +16,32 @@ public class GameCamera {
 
 		this.xOffset = 0;
 		this.yOffset = 0;
+	}
+
+	public void checkBlankSpace() {
+		if (this.xOffset < 0) {
+			this.xOffset = 0;
+		} else if (this.xOffset > facade.getWidth()) {
+			this.xOffset = facade.getWidth();
+		}
+
+		if (this.yOffset < 0) {
+			this.yOffset = 0;
+		} else if (this.yOffset > facade.getHeight()) {
+			this.yOffset = facade.getHeight();
+		}
+	}
+
+	public void centerOnEntity(Entity e) {
+		this.xOffset = e.getPositionX() + (e.getPositionWidth() / 2) - (facade.getWidth() / 2);
+		this.yOffset = e.getPositionY() + (e.getPositionHeight() / 2) - (facade.getHeight() / 2);
+		this.checkBlankSpace();
+	}
+
+	public void move(int xAmount, int yAmount) {
+		this.xOffset += xAmount;
+		this.yOffset += yAmount;
+		this.checkBlankSpace();
 	}
 
 	public int getXOffset() {
