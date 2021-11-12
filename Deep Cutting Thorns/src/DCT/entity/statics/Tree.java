279fd0fe5beb7f532df2b8c5cd26763945467de6
Alessio <alessiomalfissi99@outlook.it>
2021-11-12 18:16:41 +0100

    0049 - Tree render according to camera

--- Deep Cutting Thorns/src/DCT/entity/statics/Tree.java
+++ Deep Cutting Thorns/src/DCT/entity/statics/Tree.java
@@ -28,7 +28,7 @@ public class Tree extends StaticEntity {
 
 	@Override
 	public void render(Graphics g) {
-		g.drawImage(Assets.tree, this.x, this.y, TREEWIDTH * SCALE, TREEHEIGHT * SCALE, null);
+		g.drawImage(Assets.tree, this.x - this.facade.getGameCamera().getXOffset(), this.y - this.facade.getGameCamera().getYOffset(), TREEWIDTH * SCALE, TREEHEIGHT * SCALE, null);
 		super.render(g);
 	}
 
