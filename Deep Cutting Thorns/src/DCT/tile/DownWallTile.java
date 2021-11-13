71fd327bc086140f489bb3d24c5f446ccb4b66c5
Einar4 <andreasbabo6@gmail.com>
2021-11-14 00:41:02 +0100

    0050 - added hitbox to solid tile

--- Deep Cutting Thorns/src/DCT/tile/DownWallTile.java
+++ Deep Cutting Thorns/src/DCT/tile/DownWallTile.java
@@ -1,12 +1,14 @@
 package DCT.tile;
 
+import java.awt.Color;
+
 import DCT.gfx.Assets;
 
 public class DownWallTile extends Tile {
 
 	public DownWallTile(int id) {
 		super(Assets.downWall, id);
-		// TODO Auto-generated constructor stub
+		this.setDebuggingColor(new Color(60, 60, 60));
 	}
 	
 	@Override
--- Deep Cutting Thorns/src/DCT/tile/UpperColumnsTile.java
+++ Deep Cutting Thorns/src/DCT/tile/UpperColumnsTile.java
@@ -1,13 +1,16 @@
 package DCT.tile;
 
+import java.awt.Color;
+
 import DCT.gfx.Assets;
 
 public class UpperColumnsTile extends Tile {
 
 	public UpperColumnsTile(int id) {
 		super(Assets.upperColumns, id);
+		this.setDebuggingColor(new Color(30, 30, 30));
 	}
-	
+
 	@Override
 	public boolean isSolid() {
 		return true;
