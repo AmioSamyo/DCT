28fe2710d90c293118a16d40eacadfbd8754111d
Loggi <flaviog2@hotmail.it>
2021-11-13 23:39:08 +0100

    0039 - key manager roll

--- Deep Cutting Thorns/src/DCT/input/KeyManager.java
+++ Deep Cutting Thorns/src/DCT/input/KeyManager.java
@@ -5,7 +5,7 @@ import java.awt.event.KeyListener;
 
 public class KeyManager implements KeyListener {
 
-	private boolean up, down, left, right, sprint;
+	private boolean up, down, left, right, sprint, roll;
 	private boolean debugMode;
 	private boolean[] keys, justPressed, cantPress;
 
@@ -37,6 +37,7 @@ public class KeyManager implements KeyListener {
 		this.left = this.keys[KeyEvent.VK_A];
 		this.right = this.keys[KeyEvent.VK_D];
 		this.sprint = this.keys[KeyEvent.VK_SHIFT];
+		this.roll = this.keys[KeyEvent.VK_SPACE];
 
 		this.debugMode = this.keys[KeyEvent.VK_P];
 
@@ -99,5 +100,9 @@ public class KeyManager implements KeyListener {
 	
 	public boolean getSprint() {
 		return this.sprint;
+	}
+	
+	public boolean getRoll() {
+		return this.roll;
 	}
 }
