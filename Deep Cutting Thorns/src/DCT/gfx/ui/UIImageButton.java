6b15041f5127353f830d6a1d657305e429e018a5
Alessio <alessiomalfissi99@outlook.it>
2021-11-15 21:41:37 +0100

    0053 - UIImageButton class

--- Deep Cutting Thorns/src/DCT/gfx/ui/UIImageButton.java
+++ Deep Cutting Thorns/src/DCT/gfx/ui/UIImageButton.java
@@ -1,9 +1,41 @@
 package DCT.gfx.ui;
 
-public class UIImageButton {
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 
-	public UIImageButton() {
-		// TODO Auto-generated constructor stub
+import DCT.utility.Rectangle;
+
+public class UIImageButton extends UIObject {
+
+	private BufferedImage[] images;
+	private IClickListener clicker;
+
+	public UIImageButton(Rectangle dimensions, BufferedImage[] images, IClickListener clicker) {
+		super(dimensions);
+		this.images = images;
+		this.clicker = clicker;
+	}
+	
+	@Override
+	public void onClick() {
+		this.clicker.onClick();
+	}
+
+	@Override
+	public void render(Graphics g) {
+		if (hovering) {
+			g.drawImage(this.images[1], this.getPositionX(), this.getPositionY(), this.getPositionWidth(),
+					this.getPositionHeight(), null);
+		} else {
+			g.drawImage(this.images[0], this.getPositionX(), this.getPositionY(), this.getPositionWidth(),
+					this.getPositionHeight(), null);
+		}
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+
 	}
 
 }
