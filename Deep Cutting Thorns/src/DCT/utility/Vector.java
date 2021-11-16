2959c18fb2bded4e542ddd598de7eb778a5b270e
Loggi <flaviog2@hotmail.it>
2021-11-16 21:55:27 +0100

    0039 - vector util

---
+++ Deep Cutting Thorns/src/DCT/utility/Vector.java
@@ -0,0 +1,19 @@
+package DCT.utility;
+
+public class Vector {
+
+	private int x, y;
+	
+	public Vector(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+}
