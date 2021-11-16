81f678450711426371c2bb03791d672968b5dd8f
LoggiExp <92399059+LoggiExp@users.noreply.github.com>
2021-11-16 23:13:18 +0100

    Loggi/dct 0039 sb => develop (#34)
    * 0039 - added shift key for sprint
    
    * 0039 - player sprint added
    
    * 0039 - sprint animation refactor
    
    * 0039 - key manager roll
    
    * 0039 - player sprint refactor
    
    * 0039 - refactor player animation
    
    * 0039 - refactor getimput
    
    * 0039 - roll method
    
    * 0039 - roll animation(roccioso)
    
    * 0039 - fixes
    
    * 0039 - roll method
    
    * 0039 - justPressedTimed //TODO
    
    * 0039 - non funzizona dio madonna
    
    * 0039 - vector util
    
    * 0039 - roll base setup, //TODO take directions
    
    * 0039 - previousDirection
    
    * 0039 - rolling in the deep
    
    * 0039 - last adjustment to roll action
    
    * 0039 - fix

--- Deep Cutting Thorns/src/DCT/entity/Player.java
+++ Deep Cutting Thorns/src/DCT/entity/Player.java
@@ -2,6 +2,7 @@ package DCT.entity;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
 
 import DCT.Facade;
 import DCT.gfx.Animation;
@@ -11,18 +12,26 @@ import DCT.utility.Rectangle;
 public class Player extends Creature {
 
 	private int currentHealth;
+	private boolean isRolling;
+	private int rollCurrentDistance;
 
 	private Animation playerMoveDown, playerMoveRight, playerMoveUp, playerMoveLeft, playerIdle, deadAnimation;
+	private Animation playerSprintDown, playerSprintRight, playerSprintUp, playerSprintLeft;
+	private Animation playerRoll;
 	private Animation currentAnimation;
 
 	private static final int PLAYERWIDTH = 704 / 11, PLAYERHEIGHT = 320 / 5;
 	private static final int SCALE = 2;
-	private static final int ANIMATIONSPEED = 100;
+	private static final int ANIMATIONSPEED = 100, ANIMATIONSPRINTSPEED = 60;
+	private static final int SPRINTSPEED = 8, ROLLBASEDISTANCE = 17;
+	private static final double ROLLDELTA = 0.1;
 
 	public Player(Facade facade, int x, int y) {
 
 		super(facade, new Rectangle(x, y, PLAYERWIDTH * SCALE, PLAYERHEIGHT * SCALE));
 		this.speed = 4;
+		this.isRolling = false;
+		this.rollCurrentDistance = this.ROLLBASEDISTANCE;
 
 		initialize();
 	}
@@ -50,6 +59,13 @@ public class Player extends Creature {
 		this.playerIdle = new Animation(ANIMATIONSPEED, Assets.playerAnimationIdle);
 		this.deadAnimation = new Animation(1, Assets.playerDeadAnimation);
 
+		this.playerSprintDown = new Animation(ANIMATIONSPRINTSPEED, Assets.playerAnimationDown);
+		this.playerSprintRight = new Animation(ANIMATIONSPRINTSPEED, Assets.playerAnimationRight);
+		this.playerSprintUp = new Animation(ANIMATIONSPRINTSPEED, Assets.playerAnimationUp);
+		this.playerSprintLeft = new Animation(ANIMATIONSPRINTSPEED, Assets.playerAnimationLeft);
+
+		this.playerRoll = new Animation(0, Assets.playerAnimationRoll);
+
 		this.currentAnimation = this.playerIdle;
 		this.setDebuggingColor(new Color(255, 102, 255));
 	}
@@ -59,26 +75,32 @@ public class Player extends Creature {
 		if (this.health <= 0) {
 			this.die();
 		}
-
+		
 		this.playerIdle.update();
 		this.playerMoveUp.update();
 		this.playerMoveRight.update();
 		this.playerMoveLeft.update();
 		this.playerMoveDown.update();
 
+		this.playerSprintUp.update();
+		this.playerSprintRight.update();
+		this.playerSprintLeft.update();
+		this.playerSprintDown.update();
+
+		this.playerRoll.update();
+
 		this.currentAnimation.update();
 
 		if (this.health > 0) {
 			this.playerMovement();
 		}
-
 	}
 
 	@Override
 	public void render(Graphics g) {
 		g.drawImage(this.currentAnimation.getCurrentFrame(), this.xMoveWithCamera(), this.yMoveWithCamera(),
 				this.position.getWidth(), this.position.getHeight(), null);
-		
+
 		if (this.facade.isDebugging()) {
 			Rectangle hitBox = this.getCollisionHitBox(0, 0);
 			g.setColor(this.debuggingColor);
@@ -98,40 +120,140 @@ public class Player extends Creature {
 
 	private void playerMovement() {
 		this.getInput();
-		this.move();
+
+		if (this.isRolling) {
+			this.rollingMove();
+		} else {
+			this.move();
+		}
 		this.chooseCurrentAnimation();
 		this.resetMovement();
 
 		this.facade.getGameCamera().centerOnEntity(this);
 	}
 
+	private void rollingMove() {
+		if (!this.checkEntityCollisions(this.previousDirection.getX() * this.rollCurrentDistance,
+				this.previousDirection.getY() * this.rollCurrentDistance)) {
+
+			if (this.previousDirection.getY() < 0) {
+				this.rollUp();
+			} else if (this.previousDirection.getY() > 0) {
+				this.rollDown();
+			}
+			if (this.previousDirection.getX() < 0) {
+				this.rollLeft();
+			} else if (this.previousDirection.getX() > 0) {
+				this.rollRight();
+			}
+		}
+		this.rollCurrentDistance -= this.rollCurrentDistance * this.ROLLDELTA;
+		if (this.rollCurrentDistance < 1) {
+			this.rollCurrentDistance = this.ROLLBASEDISTANCE;
+			this.isRolling = false;
+		}
+	}
+
+	private void rollUp() {
+		int futureY = this.position.getY() + this.hitBox.getY() - this.rollCurrentDistance;
+
+		if (!this.checkCollisionWithTile(this.position.getX() + this.hitBox.getX(), futureY) && !this
+				.checkCollisionWithTile(this.position.getX() + this.hitBox.getX() + this.hitBox.getWidth(), futureY)) {
+			this.setY(this.getPositionY() - this.rollCurrentDistance);
+		}
+	}
+
+	private void rollDown() {
+		int futureY = this.position.getY() + this.hitBox.getY() + this.hitBox.getHeight() + this.rollCurrentDistance;
+
+		if (!this.checkCollisionWithTile(this.position.getX() + this.hitBox.getX(), futureY) && !this
+				.checkCollisionWithTile(this.position.getX() + this.hitBox.getX() + this.hitBox.getWidth(), futureY)) {
+			this.setY(this.getPositionY() + this.rollCurrentDistance);
+		}
+	}
+
+	private void rollLeft() {
+		int futureX = this.position.getX() + this.hitBox.getX() - this.rollCurrentDistance;
+
+		if (!this.checkCollisionWithTile(futureX, this.position.getY() + this.hitBox.getY()) && !this
+				.checkCollisionWithTile(futureX, this.position.getY() + this.hitBox.getY() + this.hitBox.getHeight())) {
+			this.setX(this.getPositionX() - this.rollCurrentDistance);
+		}
+	}
+
+	private void rollRight() {
+		int futureX = this.position.getX() + this.hitBox.getX() + this.hitBox.getWidth() + this.rollCurrentDistance;
+		
+		if (!this.checkCollisionWithTile(futureX, this.position.getY() + this.hitBox.getY()) && !this
+				.checkCollisionWithTile(futureX, this.position.getY() + this.hitBox.getY() + this.hitBox.getHeight())) {
+			this.setX(this.getPositionX() + this.rollCurrentDistance);
+		}
+	}
+
 	private void getInput() {
 		if (this.facade.getKeyManager().getUp()) {
-			this.addYMove(-this.speed);
+			this.setYSpeed(-this.speed, -SPRINTSPEED);
 		}
 		if (this.facade.getKeyManager().getDown()) {
-			this.addYMove(this.speed);
+			this.setYSpeed(this.speed, SPRINTSPEED);
 		}
 		if (this.facade.getKeyManager().getLeft()) {
-			this.addXMove(-this.speed);
+			this.setXSpeed(-this.speed, -SPRINTSPEED);
 		}
 		if (this.facade.getKeyManager().getRight()) {
-			this.addXMove(this.speed);
+			this.setXSpeed(this.speed, SPRINTSPEED);
 		}
+		if (this.facade.getKeyManager().keyJustPressed(KeyEvent.VK_SPACE) && !this.isNotMoving()) {
+			this.roll();
+		}
+	}
+
+	private void setYSpeed(int speed, int sprintSpeed) {
+		if (this.facade.getKeyManager().getSprint()) {
+			this.addYMove(sprintSpeed);
+		} else {
+			this.addYMove(speed);
+		}
+	}
+
+	private void setXSpeed(int speed, int sprintSpeed) {
+		if (this.facade.getKeyManager().getSprint()) {
+			this.addXMove(sprintSpeed);
+		} else {
+			this.addXMove(speed);
+		}
+	}
+
+	private void roll() {
+		this.isRolling = true;
 	}
 
 	private void chooseCurrentAnimation() {
-		if (this.xMove < 0) {
-			this.currentAnimation = this.playerMoveLeft;
-		} else if (this.xMove > 0) {
-			this.currentAnimation = this.playerMoveRight;
-		} else if (this.yMove < 0) {
-			this.currentAnimation = this.playerMoveUp;
-		} else if (this.yMove > 0) {
-			this.currentAnimation = this.playerMoveDown;
-		} else {
+		if (this.isMovingUp()) {
+			this.setAnimation(this.playerMoveUp, this.playerSprintUp);
+		}
+		if (this.isMovingDown()) {
+			this.setAnimation(this.playerMoveDown, this.playerSprintDown);
+		}
+		if (this.isMovingLeft()) {
+			this.setAnimation(this.playerMoveLeft, this.playerSprintLeft);
+		}
+		if (this.isMovingRight()) {
+			this.setAnimation(this.playerMoveRight, this.playerSprintRight);
+		}
+		if (this.isNotMoving()) {
 			this.currentAnimation = this.playerIdle;
+		}
+		if (this.isRolling) {
+			this.currentAnimation = this.playerRoll;
 		}
 	}
 
+	private void setAnimation(Animation anim, Animation animSprint) {
+		if (this.facade.getKeyManager().getSprint()) {
+			this.currentAnimation = animSprint;
+		} else {
+			this.currentAnimation = anim;
+		}
+	}
 }
