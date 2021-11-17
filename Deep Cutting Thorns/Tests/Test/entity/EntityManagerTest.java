d656a6a62ea0543c1295ce393b7783e1fc6d58fa
Alessio <alessiomalfissi99@outlook.it>
2021-11-17 22:30:31 +0100

    0056 - Mockito add + maven project

--- Deep Cutting Thorns/Tests/Test/entity/EntityManagerTest.java
+++ Deep Cutting Thorns/Tests/Test/entity/EntityManagerTest.java
@@ -2,6 +2,7 @@ package Test.entity;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import DCT.Facade;
 import DCT.entity.EntityManager;
@@ -13,13 +14,16 @@ class EntityManagerTest {
 	Game mockedGame = new Game("", 100, 100);
 	Facade mockedFacade = new Facade(mockedGame);
 	Player player1 = new Player(mockedFacade, 0, 0);
-	Player player2 = new Player(mockedFacade, 10, 10);
+	Player player2 = new Player(mockedFacade, 100, 100);
 
 	@Test
 	void testUpdate() {
+		Player player1 = Mockito.mock(Player.class);
 		EntityManager em = new EntityManager(player2);
 		em.addEntity(player1);
 		em.update();
+		assertEquals(em.getEntityList().get(0), player1);
+		assertEquals(em.getEntityList().get(1), player2);
 	}
 
 }
