83e13a283a6200cfd305bb770fb139b84a26e889
Alessio <alessiomalfissi99@outlook.it>
2021-11-17 23:07:29 +0100

    0056 - Rectangle contains and intercepts tests

---
+++ Deep Cutting Thorns/Tests/Test/utility/RectangleTest.java
@@ -0,0 +1,32 @@
+package Test.utility;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import DCT.utility.Rectangle;
+import DCT.utility.Vector;
+
+class RectangleTest {
+
+	@Test
+	void testContains() {
+		Rectangle rectangle = new Rectangle(0, 0, 10, 10);
+		Vector pointInside = new Vector(5, 5);
+		Vector pointOutside = new Vector(20, 20);
+		
+		assertTrue(rectangle.contains(pointInside.getX(), pointInside.getY()));
+		assertFalse(rectangle.contains(pointOutside.getX(), pointOutside.getY()));
+	}
+
+	@Test
+	void testIntersects() {
+		Rectangle rectangleBase = new Rectangle(0, 0, 10, 10);
+		Rectangle rectangleIntercepting = new Rectangle(-5, -5, 5, 5);
+		Rectangle rectangleNotIntercepting = new Rectangle(20, 20, 10, 10);
+		
+		assertTrue(rectangleBase.intersects(rectangleIntercepting));
+		assertFalse(rectangleBase.intersects(rectangleNotIntercepting));
+	}
+
+}
