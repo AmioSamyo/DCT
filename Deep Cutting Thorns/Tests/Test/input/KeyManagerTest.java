896c57254f3364907f0ac4f3d6078b3fc6fc11d3
Alessio <alessiomalfissi99@outlook.it>
2021-11-18 18:45:38 +0100

    0056 - KeyManager keyPressed, KeyReleased and keyJustPressed tests

---
+++ Deep Cutting Thorns/Tests/Test/input/KeyManagerTest.java
@@ -0,0 +1,61 @@
+package Test.input;
+
+import java.awt.event.KeyEvent;
+import java.awt.Button;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import DCT.Facade;
+import DCT.input.KeyManager;
+
+class KeyManagerTest {
+
+	@Mock
+	Facade facade;
+
+	@Mock
+	KeyManager key = new KeyManager();
+
+	@Test
+	void testKeyPressed() {
+		Button c = new Button();
+		@SuppressWarnings("deprecation")
+		KeyEvent e = new KeyEvent(c, KeyEvent.KEY_PRESSED, 5, 0, KeyEvent.VK_W);
+
+		key.keyPressed(e);
+		key.update();
+
+		assertTrue(key.getUp());
+	}
+
+	@Test
+	void testKeyReleased() {
+		Button c = new Button();
+		@SuppressWarnings("deprecation")
+		KeyEvent ePressed = new KeyEvent(c, KeyEvent.KEY_PRESSED, 5, 0, KeyEvent.VK_W);
+		@SuppressWarnings("deprecation")
+		KeyEvent eReleased = new KeyEvent(c, KeyEvent.KEY_RELEASED, 5, 0, KeyEvent.VK_W);
+
+		key.keyPressed(ePressed);
+		key.update();
+		key.keyReleased(eReleased);
+		key.update();
+
+		assertFalse(key.getUp());
+	}
+
+	@Test
+	void testKeyJustPressed() {
+		Button c = new Button();
+		@SuppressWarnings("deprecation")
+		KeyEvent e = new KeyEvent(c, KeyEvent.KEY_PRESSED, 5, 0, KeyEvent.VK_W);
+
+		key.keyPressed(e);
+		key.update();
+		
+		assertTrue(key.keyJustPressed(e.getKeyCode()));
+	}
+
+}
