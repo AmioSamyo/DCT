2d3e231f0ed9985d1ca822e7596e000a02fe1abf
Loggi <flaviog2@hotmail.it>
2021-11-19 22:29:35 +0100

    0055 - text draw String method and string refactor

--- Deep Cutting Thorns/src/DCT/gfx/tools/Text.java
+++ Deep Cutting Thorns/src/DCT/gfx/tools/Text.java
@@ -2,31 +2,34 @@ package DCT.gfx.tools;
 
 import java.awt.Graphics;
 
-import javax.swing.JTextPane;
-
 import DCT.utility.TextOption;
 
 public class Text {
 
-	private JTextPane textPane;
 	private TextOption textOption;
-	private boolean centered;
+	private Graphics g;
+	private int x, y;
 	
 	public Text(TextOption textOption, Graphics g) {
 		
 		this.textOption = textOption;
-		this.centered = false;
+		this.g = g;
 		
-		this.textPane.setText(this.textOption.getText());
-		this.textPane.setFont(this.textOption.getFont());
-		this.textPane.setForeground(this.textOption.getColor());
+		this.g.setColor(this.textOption.getColor());
+		this.g.setFont(this.textOption.getFont());
 		
+		this.setPosition();
+		this.drawString();
 	}
 	
+	private void drawString() {
+		this.g.drawString(this.textOption.getText(), this.x, this.y);
+	}
+
 	private void setPosition() {
-		if (this.centered == false) {
-			this.textPane.setAlignmentX(this.textOption.getPosition().getX());
-			this.textPane.setAlignmentY(this.textOption.getPosition().getY());
+		if (this.textOption.getCentered() == false) {
+			this.x = textOption.getPosition().getX();
+			this.y = textOption.getPosition().getY();
 		}
 		else {
 			//TODO
