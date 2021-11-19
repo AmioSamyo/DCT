23b9918eb855195c442a4f51532685547b1328fe
Loggi <flaviog2@hotmail.it>
2021-11-19 23:28:48 +0100

    0055 - drawString centered implemented

--- Deep Cutting Thorns/src/DCT/gfx/tools/Text.java
+++ Deep Cutting Thorns/src/DCT/gfx/tools/Text.java
@@ -1,5 +1,6 @@
 package DCT.gfx.tools;
 
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 
 import DCT.utility.TextOption;
@@ -17,18 +18,20 @@ public class Text {
 		g.setColor(textOption.getColor());
 		g.setFont(textOption.getFont());
 		
-		setPosition(textOption);
+		setPosition(textOption, g);
 		
 		g.drawString(textOption.getText(), x, y);
 	}
 
-	private static void setPosition(TextOption textOption) {
+	private static void setPosition(TextOption textOption, Graphics g) {
 		if (textOption.getCentered() == false) {
 			x = textOption.getPosition().getX();
 			y = textOption.getPosition().getY();
 		}
 		else {
-			//TODO
+			FontMetrics metrics = g.getFontMetrics(textOption.getFont());
+			x = textOption.getPosition().getX() - (metrics.stringWidth(textOption.getText()) / 2);
+			y = textOption.getPosition().getY() + (metrics.getHeight() / 2);
 		}
 	}
 }
