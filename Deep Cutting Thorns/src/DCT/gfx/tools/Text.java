d3f170dfb3be49855b644d2749e9d1b232479173
Loggi <flaviog2@hotmail.it>
2021-11-19 21:55:17 +0100

    0055 - text setup and set position default

--- Deep Cutting Thorns/src/DCT/gfx/tools/Text.java
+++ Deep Cutting Thorns/src/DCT/gfx/tools/Text.java
@@ -1,17 +1,35 @@
 package DCT.gfx.tools;
 
+import java.awt.Graphics;
+
 import javax.swing.JTextPane;
+
+import DCT.utility.TextOption;
 
 public class Text {
 
 	private JTextPane textPane;
+	private TextOption textOption;
+	private boolean centered;
 	
-	public Text(TextOption textOption) {
-	
-		this.textPane.setText(this.text);
-		this.textPane.setFont(this.font);
-		this.textPane.setForeground(this.color);
+	public Text(TextOption textOption, Graphics g) {
+		
+		this.textOption = textOption;
+		this.centered = false;
+		
+		this.textPane.setText(this.textOption.getText());
+		this.textPane.setFont(this.textOption.getFont());
+		this.textPane.setForeground(this.textOption.getColor());
+		
 	}
 	
-
+	private void setPosition() {
+		if (this.centered == false) {
+			this.textPane.setAlignmentX(this.textOption.getPosition().getX());
+			this.textPane.setAlignmentY(this.textOption.getPosition().getY());
+		}
+		else {
+			//TODO
+		}
+	}
 }
