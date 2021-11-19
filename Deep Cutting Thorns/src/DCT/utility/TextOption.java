9cfe4437b5d5eb428f28e9ce2a19ae061214efb0
Loggi <flaviog2@hotmail.it>
2021-11-19 22:02:10 +0100

    0055 - added boolean centered in TextOption constructor

--- Deep Cutting Thorns/src/DCT/utility/TextOption.java
+++ Deep Cutting Thorns/src/DCT/utility/TextOption.java
@@ -9,12 +9,14 @@ public class TextOption {
 	private Vector position;
 	private Font font;
 	private Color color;
+	private boolean centered;
 
-	public TextOption(String text, Vector position, Font font, Color color) {
+	public TextOption(String text, Vector position, Font font, Color color, boolean centered) {
 		this.text = text;
 		this.position = position;
 		this.font = font;
 		this.color = color;
+		this.centered = centered;
 	}
 	
 	public String getText() {
@@ -31,5 +33,9 @@ public class TextOption {
 	
 	public Color getColor() {
 		return this.color;
+	}
+	
+	public boolean getCentered() {
+		return this.centered;
 	}
 }
