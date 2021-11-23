05fd1c73f4896a3d7c090d52b25b0682b68d5dd7
LoggiExp <92399059+LoggiExp@users.noreply.github.com>
2021-11-23 21:55:30 +0100

    Loggi/dct 0059 sb => develop (#40)
    * 0059 - Sound class
    
    * 0059 - test sound
    
    * 0059 - setVolume
    
    * 0059 - comment to see test
    
    * 0059 - fix
    
    * 0059 - fix
    
    * 0059 - fix

---
+++ Deep Cutting Thorns/src/DCT/gfx/tools/Sound.java
@@ -0,0 +1,63 @@
+package DCT.gfx.tools;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class Sound {
+
+	private Clip clip;
+
+	public Sound(String path) {
+
+		try {
+			File file = new File(path);
+			if (file.exists()) {
+				AudioInputStream sound = AudioSystem.getAudioInputStream(file);
+				clip = AudioSystem.getClip();
+				clip.open(sound);
+			} else {
+				throw new RuntimeException("Sound: file not found: " + path);
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Sound: Malformed URL: " + e);
+		} catch (UnsupportedAudioFileException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Sound: Unsupported Audio File: " + e);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Sound: Input/Output Error: " + e);
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Sound: Line Unavailable Exception Error: " + e);
+		}
+	}
+
+	public void play() {
+		clip.setFramePosition(0);
+		clip.start();
+	}
+
+	public void loop() {
+		clip.loop(Clip.LOOP_CONTINUOUSLY);
+	}
+
+	public void stop() {
+		clip.stop();
+	}
+
+	public void setVolume(float volume) {
+		if (volume < 0f || volume > 1f)
+			throw new IllegalArgumentException("Volume not valid: " + volume);
+		FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+		gainControl.setValue(20f * (float) Math.log10(volume));
+	}
+}
